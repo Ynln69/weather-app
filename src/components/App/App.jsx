4e7309c319ace91cb13566fe7abc7b0fa6555c7e
Yana <yanalunina96@gmail.com>
2024-01-26 14:07:42 +0200

    start to work

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { AppContainer } from "./App.styled";
+
+import FilterLang from "../LanguageFilter/LanguageFilter";
+
+const App = () => {
+  return (
+    <AppContainer>
+      <FilterLang />
+    </AppContainer>
+  );
+};
+
+export default App;
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const AppContainer = styled.div`
+  padding: 25px 25px 0 25px;
+`;
