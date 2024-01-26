f3f3d5aa43dbd47b2ce86382c5bc4e7096103b2b
Yana <yanalunina96@gmail.com>
2024-01-26 16:26:02 +0200

    Update LanguageFilter.jsx

--- src/components/LanguageFilter/LanguageFilter.jsx
+++ src/components/LanguageFilter/LanguageFilter.jsx
@@ -1,16 +1,30 @@
+import React, { useState } from "react";
+
 import { LangContainer, LangSelect, LangOption } from "./LanguageFilter.styled";
 import svgImg from "../../images/languageSvg.svg";
 
 const FilterLang = () => {
+  const options = [
+    { value: "option1", label: "EN" },
+    { value: "option2", label: "UA" },
+    { value: "option3", label: "HE" },
+  ];
+
+  const [selectedOption, setSelectedOption] = useState(options[0].value);
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <>
       <LangContainer>
-        <LangSelect>
-          <LangOption value="option1" selected>
-            EN
-          </LangOption>
-          <LangOption value="option2">UA</LangOption>
-          <LangOption value="option3">HE</LangOption>
+        <LangSelect value={selectedOption} onChange={handleChange}>
+          {options.map((option) => (
+            <LangOption key={option.value} value={option.value}>
+              {option.label}
+            </LangOption>
+          ))}
         </LangSelect>
         <img src={svgImg} alt="" width={13} height={13} />
       </LangContainer>
